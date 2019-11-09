07aeb7ab5f809027b0df6883d1d04d6f0b84e612
Rosarlin Thai <rosarlin@eaze.com>
2019-11-08 16:51:41 -0800

    working: gets dispensaries

--- api/requests.js
+++ api/requests.js
@@ -1,77 +1,77 @@
-import fetch from 'isomorphic-unfetch';
+import fetch from 'isomorphic-unfetch'
 
-async function signIn(email, password) {
-  console.group('AUTHENTICATING');
-  let json = { message: 'API failed to provide a explaination.' };
+async function signIn (email, password) {
+  console.group('AUTHENTICATING')
+  let json = { message: 'API failed to provide a explaination.' }
   try {
-    const endpoint = 'https://api.staging.eaze.tech/api/auth/signin';
+    const endpoint = 'https://api.staging.eaze.tech/api/auth/signin'
     const requestOptions = {
       method: 'POST',
       mode: 'cors',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         email: email,
-        password: password,
-      }),
-    };
+        password: password
+      })
+    }
 
-    const response = await fetch(endpoint, requestOptions);
-    json = await response.json();
+    const response = await fetch(endpoint, requestOptions)
+    json = await response.json()
 
-    if (!response.ok) throw json.message;
-    console.log('SUCCESS');
+    if (!response.ok) throw json.message
+    console.log('SUCCESS')
   } catch (error) {
-    console.error('Failed to authenticate:', error);
+    console.error('Failed to authenticate:', error)
   } finally {
-    console.groupEnd();
+    console.groupEnd()
   }
 
-  return json;
+  return json
 }
 
-async function assignDriverToDepot() {
-  return {};
+async function assignDriverToDepot () {
+  return {}
 }
 
-async function getAllDispensaries(dispensaryId) {
-  console.group('FETCHING DISPENSARIES');
-  let json = { message: 'API failed to provide a explaination.' };
-  const xAuthToken = 'REPLACE_WITH_AUTH_TOKEN';
+// todo: wrap token as a closure
+async function getAllDispensaries (token) {
+  console.group('FETCHING DISPENSARIES')
+  let json = { message: 'API failed to provide a explaination.' }
+  const xAuthToken = token
   try {
-    const endpoint = 'https://api.staging.eaze.tech/api/dispensaries';
+    const endpoint = 'https://api.staging.eaze.tech/api/dispensaries'
     const requestOptions = {
       method: 'GET',
       mode: 'cors',
       headers: {
         'Content-Type': 'application/json',
-        // TODOAH Make this read from cookies
-        'X-Auth-Token': xAuthToken,
-      },
-    };
-    const response = await fetch(endpoint, requestOptions);
-    json = await response.json();
+        'X-Auth-Token': xAuthToken
+      }
+    }
+    const response = await fetch(endpoint, requestOptions)
+    json = await response.json()
 
-    if (!response.ok) throw json.message;
-    console.log('SUCCESS');
+    if (!response.ok) throw json.message
+    console.log('SUCCESS')
   } catch (error) {
-    console.error('Failed to fetch dispensaries:', error);
+    console.error('Failed to fetch dispensaries:', error)
   } finally {
-    console.groupEnd();
+    console.groupEnd()
   }
 
-  return json;
+  return json
 }
 
-async function getDepotsForDispensary(dispensaryId) {
-  const dispensaries = await getAllDispensaries();
-  const depots = findDepotsForDispensary(dispensaryId, dispensaries);
-  return depots.filter(depot => depot.isEnabled);
+async function getDepotsForDispensary (token, dispensaryId) {
+  const dispensaries = await getAllDispensaries(token)
+  const depots = findDepotsForDispensary(dispensaryId, dispensaries)
+  return depots.filter((depot) => depot.isEnabled)
 }
 
-async function getUserId() {
-  return {};
+async function getUserId () {
+  return {}
 }
 
 /**
@@ -82,16 +82,16 @@ async function getUserId() {
  * @param {Array} dispensaries
  * @returns {Array} depots
  */
-function findDepotsForDispensary(dispensaryId, dispensaries = []) {
+function findDepotsForDispensary (dispensaryId, dispensaries = []) {
   try {
-    const { depots = [] } = dispensaries.find(disp => disp.id === dispensaryId);
-    return depots;
+    const { depots = [] } = dispensaries.find((disp) => disp.id === dispensaryId)
+    return depots
   } catch (error) {
     console.error(
       `An error was encountered while searching for depots related to Dispensary ID: ${dispensaryId}`,
-      error,
-    );
-    return [];
+      error
+    )
+    return []
   }
 }
 
@@ -100,5 +100,5 @@ export {
   getAllDispensaries,
   getDepotsForDispensary,
   getUserId,
-  signIn,
-};
+  signIn
+}
